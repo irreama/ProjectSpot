f2ed3d2a2414f9497fe16a8b409d74cd54c1a51e
afisherwpi <afisher@wpi.edu>
2013-11-04 10:50:13 -0500

    Fixing Error

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -11,7 +11,8 @@ class Pages extends CI_Controller{
 		$data['title'] = ucfirst($page); //Capitalize the first letter
 
 		$this->load->view('templates/header', $data);
-		$this->load->view('pages')
+		$this->load->view('pages/'.$page, $data);
+		$this->load->view('templates/footer', $data);
 	}
 }
 
