50e4ba905987e66c829f96e2a073e9bca1003038
Madalyn Coryea <madalyn@peachplex.com>
2013-12-15 13:50:05 -0500

    some new files added too

---
+++ application/views/pages/browse.php
@@ -0,0 +1,8 @@
+<div class="page-title">
+	<h2>Browse</h2>
+</div>
+<ul>
+<a href="/index.php/student"><li>Madalyn Coryea</li></a>
+<a href="/index.php/student"><li>Anthony Fisher</li></a>
+<a href="/index.php/student"><li>Joe Dow</li></a>
+</ul>
