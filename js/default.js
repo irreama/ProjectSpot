e76b64b9178703d9bce1f776d371396728ebf475
Anthony Fisher <irreama@gmail.com>
2013-12-15 16:42:27 -0500

    debugging

--- js/default.js
+++ js/default.js
@@ -6,8 +6,9 @@ $('.tag_listing').on('change', 'input[type="checkbox"]', function(){
 	console.log("Checkbox Changed");
 	if($(this).is(':checked')){
 		//Add it to the form
-		console.log($(this).parent().html());
+		
 		var new_box = '<input type="checkbox" data-id="' + $(this).data('id') + '" value="' + $(this).data('id')+'" checked>' + $(this).data('text');
+		console.log(new_box);
 		$('.tag_form').append('<div class="tag_item">' + new_box +"</div>");
 	}
 	else{
