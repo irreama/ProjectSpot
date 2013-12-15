77126ec47ecf42ccee5ccccbab3fff1428efd886
Anthony Fisher <irreama@gmail.com>
2013-12-15 16:14:12 -0500

    Fixing this

--- js/default.js
+++ js/default.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
 //Handle the Interests Page
 
 $('.tag_item input[type="checkbox"]').change(function(){
-	if(this.checked){
+	if($(this).checked){
 		//Add it to the form
 		
-		$('.tag_form').append('<div class="tag_item">' + this.parent().html() +"</div>");
+		$('.tag_form').append('<div class="tag_item">' + $(this).parent().html() +"</div>");
 	}
 	else{
 		//Remove it from the form
-		var id = this.data('id');
+		var id = $(this).data('id');
 		$('.tag_form div input[type="checkbox"][data-id="' + id +'"]').parent().remove();
 	}
 })
