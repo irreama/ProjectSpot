1f368f8f0a04779f16ab50f88fadce248b8471f9
Anthony Fisher <irreama@gmail.com>
2013-12-14 19:30:52 -0500

    Adding Madalyn's Templates for Header and Footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,11 @@
-		<strong>&copy; 2013</strong>
+			</div><!--End content-->
+			<!--strong>&copy; 2013</strong-->
+			<div class="footer">
+				<div class="footer_content">
+					<p>WPI</p>
+					<p>ProjectSpot &copy; 2013</p>
+				</div>
+			</div>
+		</div><!-- End container -->
 	</body>
 </html>
