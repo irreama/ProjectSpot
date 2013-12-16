da29b818f9cdad05ef9deeefd846fe6dbab1c8a3
Madalyn Coryea <madalyn@peachplex.com>
2013-12-16 12:38:17 -0500

    oops href not src

--- application/views/group/landing.php
+++ application/views/group/landing.php
@@ -1,10 +1,10 @@
 <h2>You are not yet in a group.</h2>
 
 <div>
-	<a class="button-element row" src="/index.php/group/new">Create a Group Profile</a>
+	<a class="button-element row" href="/index.php/group/new">Create a Group Profile</a>
 	<a class="help">?</a>
 </div>
 <div>
-	<a class="button-element row" src="/index.php/browse">Find a Group</a>
+	<a class="button-element row" href="/index.php/browse">Find a Group</a>
 	<a class="help">?</a>
 </div>
