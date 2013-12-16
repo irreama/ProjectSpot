d50872aef3a1f53d39d15103c234e30b7db1e28b
Madalyn Coryea <madalyn@peachplex.com>
2013-12-16 15:45:52 -0500

    targets for main res page

--- application/views/home/resources.php
+++ application/views/home/resources.php
@@ -3,9 +3,9 @@
 <p>Below are a list of resources pertaining to the WPI MQP that you may find useful.</p>
 
 <ul>
-	<li><a href="http://www.wpi.edu/academics/cs/mqp.html">The Computer Science MQP</a></li>
-	<li><a href="https://www.wpi.edu/offices/registrar/forms.html">Registrar Forms</a></li>
-	<li><a href="http://www.wpi.edu/Pubs/E-project/?shortURL=eprojects&redirector=plus.wpi.edu">The WPI eProjects Page</a></li>
-	<li><a href="http://www.wpi.edu/Pubs/E-project/browse/mqp_by_department/c.html">Electronic Projects Collection</a></li>
-	<li><a href="http://www.wpi.edu/academics/igsd/project-centers.html">The Global MQP at WPI</a></li>
+	<li><a href="http://www.wpi.edu/academics/cs/mqp.html" target="_blank">The Computer Science MQP</a></li>
+	<li><a href="https://www.wpi.edu/offices/registrar/forms.html" target="_blank">Registrar Forms</a></li>
+	<li><a href="http://www.wpi.edu/Pubs/E-project/?shortURL=eprojects&redirector=plus.wpi.edu" target="_blank">The WPI eProjects Page</a></li>
+	<li><a href="http://www.wpi.edu/Pubs/E-project/browse/mqp_by_department/c.html" target="_blank">Electronic Projects Collection</a></li>
+	<li><a href="http://www.wpi.edu/academics/igsd/project-centers.html" target="_blank">The Global MQP at WPI</a></li>
 </ul>
