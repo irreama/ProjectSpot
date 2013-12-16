b16bf3fcacdcf40a231e7216c858e83a76317d55
Anthony Fisher <irreama@gmail.com>
2013-12-16 00:04:45 -0500

    By bye banners

--- application/views/templates/homeheader.php
+++ application/views/templates/homeheader.php
@@ -10,19 +10,5 @@
 	</head>
 	
 	<body>
-		<div id="container">
-			<div class="banner">
-				<h1 class="clearfix">ProjectSpot</h1>
-				<div class="nav clearfix">
-					<ul>
-						<li><a href="/index.php/main"><img class="nav-image" src="/images/home_icon.png"><span>Dashboard</span></a></li> <!--Change to /-->
-						<li><a href="/index.php/profile/view/4"><img class="nav-image" src="/images/profile_icon.png"><span>Profile</span></a></li> <!--student, advisor pages (no profile template?)/-->
-						<li><a href="/index.php/group"><img class="nav-image" src="/images/group_icon.png"><span>Group</span></a></li>
-						<li><a href="/index.php/browse"><img class="nav-image" src="/images/find_icon.png"><span>Find</span></a></li>
-						<li><a href="/index.php/browse"><img class="nav-image" src="/images/mail_icon.png"><span>Invites</span></a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="welcome">Welcome User, <a href="/index.php/home">Logout</a></div>
-			
+		<div id="container">			
 			<div class="content">
