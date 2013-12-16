4373152404157d6453d371cf26aefef5a8e26bb5
irreama <irreama@gmail.com>
2013-12-16 12:11:32 -0500

    Fix footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,10 +1,12 @@
 			</div><!--End content-->
-			<!--strong>&copy; 2013</strong-->
 			<div class="footer">
 				<div class="footer_content">
-					<p>WPI</p>
-					<p>ProjectSpot &copy; 2013</p>
-				</div>
+					<img class="wpi right-foot" src="/images/WPI_white.png" alt="wpi logo"/>
+					<div class="left-foot">
+						<p>WPI</p>
+						<p>ProjectSpot &copy; 2013</p>
+					</div>
+				</div><p class="clear"></p>
 			</div>
 		</div><!-- End container -->
 	</body>
