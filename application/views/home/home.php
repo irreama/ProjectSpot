7c40154b75d6eaa8b3df51737917819f8b2cd82b
Madalyn Coryea <madalyn@peachplex.com>
2013-12-20 16:17:19 -0500

    new welcome text

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -4,18 +4,16 @@
 <img class="gompei" src="/images/gompei_pspot_firstdraft.png" alt="pspot logo"/>
 <div class="welcome-text">
 	<p>
-		Welcome to ProjectSpot! ProjectSpot is a place for you to find MQP groups. 
-		Search through individual students looking for MQPs, or search through project 
-		groups that still need members. You can also see advisors and the projects they 
-		are offering that they need students for.
+		Welcome to ProjectSpot! ProjectSpot is a website for WPI Computer Science, Robotics, and 
+		IMGD students who are looking for MQP groups, partners, and advisors. 
 	</p>
 	<p>
-		To get started with ProjectSpot, login through CAS with  your WPI username and password.
+		You can browse through individual students looking for MQPs, or search for project 
+		groups that still need members. You can also see advisors and the projects that they 
+		are offering.
 	</p>
-
 	<p>
-		ProjectSpot is for both students and advisors! Advisors can set up multiple MQP groups
-		looking for members in ProjectSpot. Students can only be a part of one MQP group at a time.
+		To use ProjectSpot, login with your WPI username and password.
 	</p>
 	<p>
 		Just click login below to get started!
