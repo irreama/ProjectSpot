b04a80d45ac6a7e0c7002064a3ac260b23988b3a
Madalyn Coryea <madalyn@peachplex.com>
2013-12-20 16:04:59 -0500

    trial

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -9,26 +9,28 @@
 			</ul>
 		</div>
 	</div>
-	<h1>Welcome to ProjectSpot</h1>
-	<img class="gompei" src="/images/gompei_pspot_firstdraft.png" alt="pspot logo"/>
-	<div class="welcome-text">
-		<p>
-			Welcome to ProjectSpot! ProjectSpot is a place for you to find MQP groups. 
-			Search through individual students looking for MQPs, or search through project 
-			groups that still need members. You can also see advisors and the projects they 
-			are offering that they need students for.
-		</p>
-		<p>
-			To get started with ProjectSpot, login through CAS with  your WPI username and password.
-		</p>
+	<div class="content">
+		<h1>Welcome to ProjectSpot</h1>
+		<img class="gompei" src="/images/gompei_pspot_firstdraft.png" alt="pspot logo"/>
+		<div class="welcome-text">
+			<p>
+				Welcome to ProjectSpot! ProjectSpot is a place for you to find MQP groups. 
+				Search through individual students looking for MQPs, or search through project 
+				groups that still need members. You can also see advisors and the projects they 
+				are offering that they need students for.
+			</p>
+			<p>
+				To get started with ProjectSpot, login through CAS with  your WPI username and password.
+			</p>
 
-		<p>
-			ProjectSpot is for both students and advisors! Advisors can set up multiple MQP groups
-			looking for members in ProjectSpot. Students can only be a part of one MQP group at a time.
-		</p>
-		<p>
-			Just click login below to get started!
-		</p>
-	</div><p class="clear"></p>
-	<a class="button-element" href="/index.php/main">Login</a>
+			<p>
+				ProjectSpot is for both students and advisors! Advisors can set up multiple MQP groups
+				looking for members in ProjectSpot. Students can only be a part of one MQP group at a time.
+			</p>
+			<p>
+				Just click login below to get started!
+			</p>
+		</div><p class="clear"></p>
+		<a class="button-element" href="/index.php/main">Login</a>
+	</div><!--End Content-->
 </div><!--End Container-->
