a459e489e950c4bd1664756e593ce7a784efd951
Madalyn Coryea <madalyn@peachplex.com>
2013-12-20 16:20:19 -0500

    rearranged resources links

--- application/views/home/resources.php
+++ application/views/home/resources.php
@@ -5,7 +5,7 @@
 <ul>
 	<li><a href="http://www.wpi.edu/academics/cs/mqp.html" target="_blank">The Computer Science MQP</a></li>
 	<li><a href="https://www.wpi.edu/offices/registrar/forms.html" target="_blank">Registrar Forms</a></li>
-	<li><a href="http://www.wpi.edu/Pubs/E-project/?shortURL=eprojects&redirector=plus.wpi.edu" target="_blank">The WPI eProjects Page</a></li>
 	<li><a href="http://www.wpi.edu/Pubs/E-project/browse/mqp_by_department/c.html" target="_blank">Electronic Projects Collection</a></li>
+	<li><a href="http://www.wpi.edu/Pubs/E-project/?shortURL=eprojects&redirector=plus.wpi.edu" target="_blank">The WPI eProjects Page</a></li>
 	<li><a href="http://www.wpi.edu/academics/igsd/project-centers.html" target="_blank">The Global MQP at WPI</a></li>
 </ul>
