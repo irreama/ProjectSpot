e134222bd6bc63feddcc9d1f22760bc846b3396b
Madalyn Coryea <madalyn@peachplex.com>
2013-12-20 11:50:17 -0500

    gahh freaking break tags anthonyyyyy

--- application/views/group/edit.php
+++ application/views/group/edit.php
@@ -19,29 +19,34 @@
 	echo form_hidden('id', $group_item['id']);
 	?>
 	<div class="info">
-		<label class="title">Title:</label><input type="text" name="group_name" value="<?php echo $group_item['group_name'];?>"/><br/>
-		<?php
-		foreach($group_item['users'] as $a_user){
-		?>
-		<label class="title"><?php echo($a_user['user_status'] == "Advisor" ? "Advisor" : $a_user['user_major1']['major_text']);?>:</label><label><?php echo $a_user['user_first_name'];?> <?php echo $a_user['user_last_name'];?></label><br/>
-		<?php
-		}
-		?>
-		
-		<br/>
-		<label class="title">Interests:</label><br/>
-		<?php
-			foreach ($group_item['tags'] as $tag){
-				?>
-				<p><?php echo $tag['tag_text']?></p>
-				<?php
+		<div>
+			<label class="title">Project Title:</label><input type="text" name="group_name" value="<?php echo $group_item['group_name'];?>"/>
+			<?php
+			foreach($group_item['users'] as $a_user){
+			?>
+			<label class="title"><?php echo($a_user['user_status'] == "Advisor" ? "Advisor" : $a_user['user_major1']['major_text']);?>:</label><label><?php echo $a_user['user_first_name'];?> <?php echo $a_user['user_last_name'];?></label>
+			<?php
 			}
-		?>
-		<a class="button-element-small" href="/index.php/group/interests/edit/<?php echo $group_item['id']?>">Edit Interests</a>
-		<label class="title">Description:</label>
-		<textarea rows=5 cols=50 name="group_description"><?php echo $group_item['group_description'];?></textarea name="group_description"><br/>
-		<br/>
-		<label class="title">Contact:</label><input type="text" name="group_contact" value="<?php echo $group_item['group_contact'];?>"/>
+			?>
+		</div>
+		<div>
+			<label class="title">Interests:</label>
+			<?php
+				foreach ($group_item['tags'] as $tag){
+					?>
+					<p><?php echo $tag['tag_text']?></p>
+					<?php
+				}
+			?>
+			<a class="button-element-small" href="/index.php/group/interests/edit/<?php echo $group_item['id']?>">Edit Interests</a>
+		</div>
+		<div>
+			<label class="title">Description:</label>
+			<textarea rows=5 cols=50 name="group_description"><?php echo $group_item['group_description'];?></textarea name="group_description">
+		</div>
+		<div>
+			<label class="title">Contact:</label><input type="text" name="group_contact" value="<?php echo $group_item['group_contact'];?>"/>
+		</div>
 		<div class="button_panel">
 			<a class="button-element-small cancel" href="/index.php/group/view/<?php echo $group_item['id']?>">Cancel</a>
 			<input class="edit" type="submit" name="submit" value="Save" />
