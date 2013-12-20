3fc3b394e35b181101fd9a27e8560dbd515512f2
Madalyn Coryea <madalyn@peachplex.com>
2013-12-20 13:16:44 -0500

    more code formatting.

--- application/views/group/edit.php
+++ application/views/group/edit.php
@@ -32,6 +32,7 @@
 			?>
 			</div>
 		</div>
+		
 		<div class="interests">
 			<label class="title">Interests:</label>
 			<?php
@@ -43,13 +44,18 @@
 			?>
 			<a class="button-element-small" href="/index.php/group/interests/edit/<?php echo $group_item['id']?>">Edit Interests</a>
 		</div>
+		
 		<div class="description">
 			<label class="title">Give a description of your group:</label>
-			<textarea class="input_field full_width" rows=5 cols=50 name="group_description"><?php echo $group_item['group_description'];?></textarea name="group_description">
+			<textarea class="input_field full_width" rows=5 cols=50 name="group_description">
+				<?php echo $group_item['group_description'];?>
+			</textarea>
 		</div>
+		
 		<div class="contact">
 			<label class="title">Contact:</label><input class="input_field input_width" type="text" name="group_contact" value="<?php echo $group_item['group_contact'];?>"/>
 		</div>
+		
 		<div class="button_panel">
 			<a class="button-element-small cancel" href="/index.php/group/view/<?php echo $group_item['id']?>">Cancel</a>
 			<input class="button-element-small edit" type="submit" name="submit" value="Save" />
--- application/views/profile/edit.php
+++ application/views/profile/edit.php
@@ -6,58 +6,94 @@
 
 <div class="right_col">
 	<div class="info">
-		<?php
-		//Start Generating form fields and their data
-		echo form_open('profile/edit/'.$profile_item['id']);
-		//Hidden ID Field
-		echo form_hidden('id', $profile_item['id']);
+		<div class="basic_info">
+			<?php
+			//Start Generating form fields and their data
+			echo form_open('profile/edit/'.$profile_item['id']);
+			//Hidden ID Field
+			echo form_hidden('id', $profile_item['id']);
+			?>
+			
+			<div class="name">
+				<label class="title">Name:</label>
+				<label>
+					<?php echo $profile_item['user_first_name']?> <?php echo $profile_item['user_last_name']?>
+				</label>
+			</div>
+			
+			<div class="major">
+				<?php
+				if($profile_item['user_status'] == 'Advisor'){
+				?>
+				
+				<label class="title">Department:</label>
+				<label><?php echo $profile_item['user_major1']['major_text'] ?></label>
+				
+				<?php
+				}
+				else{
+				?>
+				
+				<label class="title">Major 1:</label>
+				<label><?php echo $profile_item['user_major1']['major_text'] ?></label>
+				<label class="title">Major 2:</label>
+				<label><?php echo $profile_item['user_major2']['major_text'] ?></label>
+				
+				<?php
+				}
 
-		?>
-		<label class="title">Name:</label><label><?php echo $profile_item['user_first_name']?> <?php echo $profile_item['user_last_name']?></label><br/>
-		<?php
-		if($profile_item['user_status'] == 'Advisor'){
-		?>
-		<label class="title">Department:</label><label><?php echo $profile_item['user_major1']['major_text'] ?></label><br/>
-		<?php
-		}
-		else{
-		?>
-		<label class="title">Major 1:</label><label><?php echo $profile_item['user_major1']['major_text'] ?></label><br/>
-		<label class="title">Major 2:</label><label><?php echo $profile_item['user_major2']['major_text'] ?></label><br/>
-		<?php
-		}
+				if($profile_item['user_status'] != 'Advisor'){
+				?>
+			</div>
+			
+			<div class="grad_year">
+				<label class="title">Graduation Year:</label>
+				<label><?php echo $profile_item['user_grad_year'] ?></label>
+				<?php
+				}
+				?>
+			</div>
+			
+			<div class="gender">
+				<label class="title">Gender:</label>
+				<label><input name="user_gender" type="radio" value="Female"<?php echo($profile_item['user_gender'] == "Female" ? " checked" : "")?>/>Female</label>
+				<label><input name="user_gender" type="radio" value="Male"<?php echo($profile_item['user_gender'] == "Male" ? " checked" : "")?>/>Male</label>
+				<label><input name="user_gender" type="radio" value="Not Provided"<?php echo($profile_item['user_gender'] == "Not Provided" ? " checked" : "")?>/>Other/No Answer</label>
+			</div>
+		</div>
 		
-		if($profile_item['user_status'] != 'Advisor'){
-		?>
-		<label class="title">Graduation Year:</label><label><?php echo $profile_item['user_grad_year'] ?></label><br/>
-		<?php
-		}
-		?>
-		<label class="title">Gender:</label>
-			<input name="user_gender" type="radio" value="Female"<?php echo($profile_item['user_gender'] == "Female" ? " checked" : "")?>/><label>Female</label>
-			<input name="user_gender" type="radio" value="Male"<?php echo($profile_item['user_gender'] == "Male" ? " checked" : "")?>/><label>Male</label>
-			<input name="user_gender" type="radio" value="Not Provided"<?php echo($profile_item['user_gender'] == "Not Provided" ? " checked" : "")?>/><label>Other/No Answer</label><br/>
-		<br/>
-		<label class="title">MQP:</label>None<br/>
-		<br/>
-		<label class="title">Interests:</label><br/>
-		<?php
-			$tag_array = array();
-			foreach ($profile_item['ps_tags'] as $tag){
-				$tag_array[] = $tag['tag_text'];
-			}
-			echo implode(',&nbsp;', $tag_array);
-		?>
-		<a href="/index.php/profile/interests/edit/<?php echo $profile_item['id']?>"><button class="edit" type="button">Edit Interests</button></a>
+		<div class="MQP">
+			<label class="title">MQP:</label>
+		<div>
 		
-		<label class="title">Describe yourself:</label>
-		<textarea class="input_field full_width" name="user_description" rows=5 cols=50><?php echo $profile_item['user_description']?></textarea>
-		<br/>
-		<label class="title">Contact:</label><label><?php echo $profile_item['user_email']?></label>
+		<div class="interests">
+			<label class="title">Interests:</label>
+			<?php
+				$tag_array = array();
+				foreach ($profile_item['ps_tags'] as $tag){
+					$tag_array[] = $tag['tag_text'];
+				}
+				echo implode(',&nbsp;', $tag_array);
+			?>
+			<a class="button-element-small" href="/index.php/profile/interests/edit/<?php echo $profile_item['id']?>">
+				Edit Interests
+			</a>
+		</div>	
+		
+		<div class="description">
+			<label class="title">Describe yourself:</label>
+			<textarea class="input_field full_width" name="user_description" rows=5 cols=50>
+				<?php echo $profile_item['user_description']?>
+			</textarea>
+		</div>
+		
+		<div class="contact">
+			<label class="title">Contact:</label><label><?php echo $profile_item['user_email']?></label>
+		</div>
 		
 		<div class="button_panel">
-			<a href="/index.php/profile/view/<?php echo $profile_item['id']?>"><button type="button" class="cancel">Cancel</button></a>
-			<input class="edit" type="submit" name="submit" value="Save" />
+			<a class="button-element-small cancel" href="/index.php/profile/view/<?php echo $profile_item['id']?>">Cancel</a>
+			<input class="button-element-small edit" type="submit" name="submit" value="Save" />
 		</div>
 
 		<?php
