4938319df426034f33b36ee8d83e3231af16b4d3
unknown <irreama@gmail.com>
2014-02-25 10:26:36 -0500

    Get user?

--- ProjectSpot/application/helpers/uservalidator_helper.php
+++ ProjectSpot/application/helpers/uservalidator_helper.php
@@ -1,6 +1,6 @@
 <?php
 function checkSession(){
-    echo getenv("CAS_UID");
+    echo getenv("REMOTE_USER");
 	$CI = & get_instance();
     //$CI->session->sess_destroy();
 	if(!$CI->session->userdata('validated')){
