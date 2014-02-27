ebcfe2477b7c2f9353786984d0aaf5b2a473f55b
Madalyn Coryea <madalyn@peachplex.com>
2014-02-27 14:07:27 -0500

    added logout button

--- ProjectSpot/application/views/templates/header.php
+++ ProjectSpot/application/views/templates/header.php
@@ -20,10 +20,11 @@ $CI =& get_instance();
 				<div class="nav clearfix">
 					<ul>
 						<li><a href="<?=base_url()?>"><img class="nav-image" src="<?=base_url()?>images/home_icon.png"><span>Dashboard</span></a></li> <!--Change to /-->
+						<li><a href="<?=base_url()?>index.php/browse"><img class="nav-image" src="<?=base_url()?>images/find_icon2.png"><span>Find</span></a></li>
 						<li><a href="<?=base_url()?>index.php/profile/view/<?=$CI->session->userdata('user_id')?>"><img class="nav-image" src="<?=base_url()?>images/profile_icon.png"><span>Profile</span></a></li> <!--student, advisor pages (no profile template?)/-->
 						<li><a href="<?=base_url()?>index.php/group"><img class="nav-image" src="<?=base_url()?>images/group_icon.png"><span>Group</span></a></li>
-						<li><a href="<?=base_url()?>index.php/browse"><img class="nav-image" src="<?=base_url()?>images/find_icon2.png"><span>Find</span></a></li>
 						<li><a href="<?=base_url()?>index.php/invites"><img class="nav-image" src="<?=base_url()?>images/mail_icon.png"><span>Invitations</span></a></li>
+						<li><a href="<?=base_url()?>index.php/logout"><span>Logout</span></a></li>
 					</ul>
 				</div>
 				<!--div class="welcome">Welcome Test User, <a class="logout" href="<?=base_url()?>index.php/home">Logout</a></div-->
