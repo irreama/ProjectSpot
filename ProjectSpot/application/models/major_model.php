d59fb0c8bf493502138f504917ab4c5561b661e5
Anthony Fisher <irreama@gmail.com>
2014-02-28 00:50:16 -0500

    Wrong field name

--- ProjectSpot/application/models/major_model.php
+++ ProjectSpot/application/models/major_model.php
@@ -70,7 +70,7 @@ class Major_model extends CI_Model{
 	}
 
 	public function getMajorIdByName($name){
-		$query = $this->db->get_where('ps_majors', array('major_name' => $name));
+		$query = $this->db->get_where('ps_majors', array('major_text' => $name));
 		$major = $query->row_array();
 		return $major['id'];
 	}
