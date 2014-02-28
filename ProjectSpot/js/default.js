01ea6f953fcbc01540aae18dade05ee436ac776d
unknown <irreama@gmail.com>
2014-02-28 14:51:55 -0500

    js for checboxes

--- ProjectSpot/js/default.js
+++ ProjectSpot/js/default.js
@@ -1,27 +1,33 @@
 $(document).ready(function(){
 
+$('input[type="checkbox"]').on('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+});
+
 //Handle the Interests Page
 $(".tag_item").on("click", function(){
 	var checkbox = $(this).children("input");
-
 	checkbox.prop("checked", !checkbox.prop("checked"));
-});
 
-$('.tag_listing').on('change', 'input[type="checkbox"]', function(){
-	console.log("Checkbox Changed");
-	if($(this).is(':checked')){
+	if(checkbox.is(':checked')){
 		//Add it to the form
 		
-		var new_box = '<input type="checkbox" name="tags[]" data-id="' + $(this).data('id') + '" value="' + $(this).data('id')+'" checked>' + $(this).data('text');
+		var new_box = '<input type="checkbox" name="tags[]" data-id="' + checkbox.data('id') + '" value="' + checkbox.data('id')+'" checked>' + checkbox.data('text');
 		console.log(new_box);
 		$('.chk_buff').before('<div class="tag_item">' + new_box +"</div>");
 	}
 	else{
 		//Remove it from the form
-		var id = $(this).data('id');
+		var id = checkbox.data('id');
 		$('.tag_form div input[type="checkbox"][data-id="' + id +'"]').parent().remove();
 		$('input[type="checkbox"][data-id="' + id +'"]').prop('checked', false);
 	}
+});
+
+$('.tag_listing').on('change', 'input[type="checkbox"]', function(){
+	console.log("Checkbox Changed");
+	
 })
 var cookieVal = $.cookie("helpText"+window.location.pathname);
 if (cookieVal == "1"){
