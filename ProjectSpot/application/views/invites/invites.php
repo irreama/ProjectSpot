d80c318f95c98f0280ce0d76130c15a4d8850a76
Madalyn Coryea <madalyn@peachplex.com>
2014-04-22 22:25:14 -0400

    fixed broken linkes to users on invites page for both Sent To advisors and students

--- ProjectSpot/application/views/invites/invites.php
+++ ProjectSpot/application/views/invites/invites.php
@@ -93,7 +93,16 @@ $in_group = $CI->group_user_rel_model->isUserInAnyGroup($CI->session->userdata("
 			?>
 			<div data-invite-id="<?=$anInvite['id']?>" class="invite">
 				<label>
-					<?=($uid == $anInvite['sender']['id'] ? "You have" : "<a href='" . base_url(). "index.php/profile/view/".$anInvite['sender']['id']."'>". $anInvite['sender']['user_first_name'] . " " . $anInvite['sender']['user_last_name'] . "</a> has") ?> invited <a href="<?=base_url()?>"><?=$anInvite['recipient']['user_first_name']?> <?=$anInvite['recipient']['user_last_name']?></a>
+					<?=($uid == $anInvite['sender']['id'] ? 
+					"You have" : 
+					"<a href='" . base_url() . "index.php/profile/view/".$anInvite['sender']['id']."'>". $anInvite['sender']['user_first_name'] . " " . $anInvite['sender']['user_last_name'] . "</a> has") 
+					?> 
+					
+					invited 
+					<a href="<?=base_url() . "index.php/profile/view/".$anInvite['recipient']['id']?>">
+						<?=$anInvite['recipient']['user_first_name']?> 
+						<?=$anInvite['recipient']['user_last_name']?>
+					</a>
 					to join your MQP group 
 				</label>
 				<a class="invite-button button-element-small inline reject">X Revoke</a>
@@ -116,7 +125,16 @@ $in_group = $CI->group_user_rel_model->isUserInAnyGroup($CI->session->userdata("
 			?>
 			<div data-invite-id="<?=$anInvite['id']?>" class="invite">
 				<label>
-					<?=($uid == $anInvite['sender']['id'] ? "You have" : "<a href='" . base_url() ."index.php/profile/view/".$anInvite['sender']['id']."'>". $anInvite['sender']['user_first_name'] . " " . $anInvite['sender']['user_last_name'] . "</a> has") ?> invited <a href="<?=base_url()?>"><?=$anInvite['recipient']['user_first_name']?> <?=$anInvite['recipient']['user_last_name']?></a>
+					<?=($uid == $anInvite['sender']['id'] ? "You have" : 
+					"<a href='" . base_url() . "index.php/profile/view/".$anInvite['sender']['id']."'>". 
+						$anInvite['sender']['user_first_name'] . " " . $anInvite['sender']['user_last_name'] . 
+					"</a> has") ?> 
+					
+					invited 
+					<a href="<?=base_url() . "index.php/profile/view/".$anInvite['recipient']['id']?>">
+						<?=$anInvite['recipient']['user_first_name']?> 
+						<?=$anInvite['recipient']['user_last_name']?>
+					</a>
 					to advise your MQP group 
 				</label>
 				<a class="invite-button button-element-small inline reject">X Revoke</a>
